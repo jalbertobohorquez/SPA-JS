f337597653398411e03577ac60b63026244cce23
jalbertobohorquez <91164244+jalbertobohorquez@users.noreply.github.com>
2022-03-30 10:00:09 -0500

    commit 3

---
+++ app.js
@@ -0,0 +1,16 @@
+/*Aplicación que me guarde los datos en una tabla*/
+//declaración de variables
+var nombre;
+var apellido;
+var telefono;
+var email;
+var direccion;
+var fecha;
+var nota;
+var tabla = document.getElementById("tabla");
+var guardar = document.getElementById("guardar");
+var contador = 1;
+console.log("Javascript funcionando");
+guardar.addEventListener("click",function(){
+    console.log("Se ha dado click al boton");
+})
