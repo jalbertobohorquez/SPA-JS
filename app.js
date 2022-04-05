680f733edcdb4a9b3028d39e09ee0aef76277fd9
jalbertobohorquez <91164244+jalbertobohorquez@users.noreply.github.com>
2022-04-05 09:23:57 -0500

    validaciones

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ function agregarATabla(){
     direccion=document.getElementById("direccion").value;
     fecha=document.getElementById("fecha").value;
     nota=document.getElementById("nota").value;
+    /*
     console.log(nombre);
     console.log(apellido);
     console.log(telefono);
@@ -28,24 +29,36 @@ function agregarATabla(){
     console.log(direccion);
     console.log(fecha);
     console.log(nota);
-    var fila = tabla.insertRow(contador);
-    var celda1 = fila.insertCell(0);
-    var celda2 = fila.insertCell(1);
-    var celda3 = fila.insertCell(2);
-    var celda4 = fila.insertCell(3);
-    var celda5 = fila.insertCell(4);
-    var celda6 = fila.insertCell(5);
-    var celda7 = fila.insertCell(6);
-    var celda8 = fila.insertCell(7);
-    celda1.innerHTML = nombre;
-    celda2.innerHTML = apellido;
-    celda3.innerHTML = telefono;
-    celda4.innerHTML = email;
-    celda5.innerHTML = direccion;
-    celda6.innerHTML = fecha;
-    celda7.innerHTML = nota;
-    celda8.innerHTML = '<button class="btn-danger" onclick="eliminarFila(this)">Eliminar</button>';
-    contador++;
+    */
+    if(nombre=="" || apellido=="" || telefono=="" || email=="" || direccion=="" || fecha==""){
+        alert("Todos los campos deben estar llenos");
+    }else{
+        var fila = tabla.insertRow(contador);
+        var celda1 = fila.insertCell(0);
+        var celda2 = fila.insertCell(1);
+        var celda3 = fila.insertCell(2);
+        var celda4 = fila.insertCell(3);
+        var celda5 = fila.insertCell(4);
+        var celda6 = fila.insertCell(5);
+        var celda7 = fila.insertCell(6);
+        var celda8 = fila.insertCell(7);
+        celda1.innerHTML = nombre;
+        celda2.innerHTML = apellido;
+        celda3.innerHTML = telefono;
+        celda4.innerHTML = email;
+        celda5.innerHTML = direccion;
+        celda6.innerHTML = fecha;
+        celda7.innerHTML = nota;
+        celda8.innerHTML = '<button class="btn-danger" onclick="eliminarFila(this)">Eliminar</button>';
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("telefono").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("direccion").value = "";
+        document.getElementById("fecha").value = "";
+        document.getElementById("nota").value = "";
+        contador++;
+    }
 }
 function eliminarFila(fila){
     tabla.deleteRow(fila.parentNode.parentNode.rowIndex);
