d7b7214fd642a502698d4d4c4120c823c4b89566
jalbertobohorquez <91164244+jalbertobohorquez@users.noreply.github.com>
2022-04-05 09:03:20 -0500

    agregar y eliminar

--- app.js
+++ app.js
@@ -10,7 +10,44 @@ var nota;
 var tabla = document.getElementById("tabla");
 var guardar = document.getElementById("guardar");
 var contador = 1;
+guardar.addEventListener("click",agregarATabla);
 console.log("Javascript funcionando");
-guardar.addEventListener("click",function(){
-    console.log("Se ha dado click al boton");
-})
+
+function agregarATabla(){
+    nombre=document.getElementById("nombre").value;
+    apellido=document.getElementById("apellido").value;
+    telefono=document.getElementById("telefono").value;
+    email=document.getElementById("email").value;
+    direccion=document.getElementById("direccion").value;
+    fecha=document.getElementById("fecha").value;
+    nota=document.getElementById("nota").value;
+    console.log(nombre);
+    console.log(apellido);
+    console.log(telefono);
+    console.log(email);
+    console.log(direccion);
+    console.log(fecha);
+    console.log(nota);
+    var fila = tabla.insertRow(contador);
+    var celda1 = fila.insertCell(0);
+    var celda2 = fila.insertCell(1);
+    var celda3 = fila.insertCell(2);
+    var celda4 = fila.insertCell(3);
+    var celda5 = fila.insertCell(4);
+    var celda6 = fila.insertCell(5);
+    var celda7 = fila.insertCell(6);
+    var celda8 = fila.insertCell(7);
+    celda1.innerHTML = nombre;
+    celda2.innerHTML = apellido;
+    celda3.innerHTML = telefono;
+    celda4.innerHTML = email;
+    celda5.innerHTML = direccion;
+    celda6.innerHTML = fecha;
+    celda7.innerHTML = nota;
+    celda8.innerHTML = '<button class="btn-danger" onclick="eliminarFila(this)">Eliminar</button>';
+    contador++;
+}
+function eliminarFila(fila){
+    tabla.deleteRow(fila.parentNode.parentNode.rowIndex);
+    contador--;
+}
